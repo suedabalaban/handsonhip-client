219417ac8e18b11515c541be4a3205a82c149d2b
[suedabalaban] <suedabalaban@gmail.com>
2024-08-01 17:55:53 +0300

    Integrate backend with frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import Login from './components/Login';
+import Login from './pages/Login'; // Import yolu güncellenmiş
 
 const App: React.FC = () => {
   return (
